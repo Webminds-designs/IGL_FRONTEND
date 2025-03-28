f8c43b9a510ef9ca1e069967e7e0766e5c0c9afd
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-28 12:07:59 +0530

    add loading preloader to Home component and implement loading state management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import StudyDestinations from "../components/StudyDestinations";
-import WhyChooseUs from '../components/WhyChooseUs';
-import TestimonialSlider from '../components/TestimonialSlider';
-import Footer from '../components/Footer';
+import WhyChooseUs from "../components/WhyChooseUs";
+import TestimonialSlider from "../components/TestimonialSlider";
+import Footer from "../components/Footer";
+import Preloader from "../components/Preloader";
 
 const Home = () => {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <StudyDestinations />
-            <WhyChooseUs />
-            <TestimonialSlider />
-            <Footer />
+  const [loading, setLoading] = useState(true);
 
-        </>
-    );
+  useEffect(() => {
+    if (document.readyState === "complete") {
+      setLoading(false);
+    } else {
+      const handleLoad = () => setLoading(false);
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <StudyDestinations />
+      <WhyChooseUs />
+      <TestimonialSlider />
+      <Footer />
+    </>
+  );
 };
 
-export default Home;  
+export default Home;
