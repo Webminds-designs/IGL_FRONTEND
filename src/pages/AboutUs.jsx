abfb7329e3f1eab57b0c89bc6b5074a5654725c2
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-06-18 10:09:05 +0530

    change paddings in about us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -97,7 +97,7 @@ const AboutUs = () => {
       </section>
 
       {/* Who We Are Section */}
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20 py-8 md:py-8">
+      <section className="max-w-full mx-auto p-3 sm:p-6 lg:p-14 py-8 md:py-8">
         <div className="flex items-center pt-4">
           <div className="w-12 md:w-20 border-t border-gray-500" />
           <h3 className="text-gray-500 text-sm md:text-base uppercase tracking-wide ml-2 md:ml-3">
@@ -181,7 +181,7 @@ const AboutUs = () => {
       </div>
 
       {/* Services Section */}
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20 py-8 md:py-2">
+      <section className="max-w-full mx-auto p-3 sm:p-6 lg:p-14 py-8 md:py-2">
         <div className="flex items-center pt-4">
           <div className="w-12 md:w-20 border-t border-gray-500" />
           <h3 className="text-gray-500 text-sm md:text-base uppercase tracking-wide ml-2 md:ml-3">
@@ -245,7 +245,7 @@ const AboutUs = () => {
       </section>
 
       {/* Meet Our Team */}
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-20 py-8 md:py-15">
+      <section className="max-w-full mx-auto p-3 sm:p-6 lg:p-14 py-8 md:py-15">
         <div className="flex items-center pt-4">
           <div className=" w-12 md:w-20 border-t border-gray-500" />
           <h3 className="text-gray-500 text-sm md:text-base uppercase tracking-wide ml-2 md:ml-3">
